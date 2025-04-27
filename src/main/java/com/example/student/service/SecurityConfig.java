d46ff756f26913b678ad2aa99ab264f8ae92b4c5
arunvemireddy <vemireddyarun0701@gmail.com>
2025-04-26 20:16:30 -0400

    added SecurityConfig class

---
+++ src/main/java/com/example/student/service/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.student.service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())  // disable CSRF for POST, PUT, DELETE
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults()); // Enable Basic Auth
+
+        return http.build();
+    }
+}
+
